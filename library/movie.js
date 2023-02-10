b50d2e1c0f1e34bc4270f94f2cf5efba6d5429be
yurii hlukhyi <yhlukhyi@gmail.com>
2023-02-09 20:54:01 -0800

    added cache

--- library/movie.js
+++ library/movie.js
@@ -1,23 +1,36 @@
 'use strict'
 
 const axios = require('axios');
+const cache = require('../cache')
 
 async function getMovie (request, response)  {
   let searchQuery = request.query.searchQuery;
   if (!searchQuery) {
     response.status(400).send('bad request');
   }
-  
   let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`
-  let movieResponse = await axios.get(movieUrl);
-  // console.log(movieResponse)
-  if (movieResponse) {
-    let movieArr = movieResponse.data.results.map(movie => new Movie(movie))
-    // console.log(movieResponse.data.results)
-    response.status(201).send(movieArr.slice(0, 5))
+
+  const key = 'movie' + searchQuery
+  if (cache[key] && (Date.now() - cache[key].timestamp <120000000)){
+    console.log('cash hit')
+    response.status(200).send(cache[key].data)
   }
-  else {
-    response.status(204).send('movie not found');
+  else{
+    console.log('cash misssssssssss')
+    
+    let movieResponse = await axios.get(movieUrl);
+  
+    if (movieResponse) {
+      let movieArr = movieResponse.data.results.map(movie => new Movie(movie))
+      cache[key] = {};
+      cache[key].timestamp = Date.now()
+      cache[key].data = movieArr.slice(0, 5)
+
+      response.status(201).send(movieArr.slice(0, 5))
+    }
+    else {
+      response.status(204).send('movie not found');
+    }
   }
   
 }
